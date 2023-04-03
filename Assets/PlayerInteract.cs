53bd667b4c47ee8f86eb7f3d7cb3581bbeb0dd8e
benfarrah01 <farrah01@allegheny.edu>
2023-04-03 16:36:33 -0400

    First commit, empty project and .gitignore

---
+++ Assets/PlayerInteract.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float interactRange = 2f;
+            Physics.OverlapSphere(transform.position, interactRange);
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliderArray)
+            {
+                Debug.Log(collider);
+            }
+        }
+
+    }
+}
